3d21fc396ef103acc04a4133747f6e7745a9443a
solarkraft <>
2023-09-30 01:47:21 +0200

    Create alert function

--- my-rust-module/src/lib.rs
+++ my-rust-module/src/lib.rs
@@ -1,7 +1,13 @@
 use gloo::console::log;
+use gloo::dialogs::alert;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn run() {
     log!("run invoked");
+}
+
+#[wasm_bindgen]
+pub fn wasm_alert() {
+    alert("Hello from Rust");
 }
