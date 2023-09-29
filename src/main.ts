b346ee89cb24f56d9109febf30a494f283d9b7c2
solarkraft <>
2023-09-30 01:42:53 +0200

    Explain WASM initialization

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import "./style.css"
 
+// We don't need to initialize the module because it's loaded as an ES module.
+// Browsers can't directly handle this yet, but vite-plugin-wasm can do the transformation for us.
 import { run } from "../my-rust-module/pkg/my_rust_module"
 run()
