649a563dba3c6a51602e79c7256a053bfbec78e3
solarkraft <>
2023-09-30 01:58:52 +0200

    Demo with button

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,5 @@ import "./style.css"
 // Browsers can't directly handle this yet, but vite-plugin-wasm can do the transformation for us.
 import { hello_console, hello_alert } from "../my-rust-module/pkg/my_rust_module"
 hello_console()
-hello_alert()
+
+document.getElementById("alertButton")!.addEventListener("click", () => hello_alert())
