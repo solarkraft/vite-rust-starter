be3749f091d10a010356f612ea8a2c9e830db9cd
solarkraft <>
2023-09-30 04:05:17 +0200

    Use vite-plugin-top-level-await
    Lets Vite build in peace

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite"
 import wasm from "vite-plugin-wasm"
+import topLevelAwait from "vite-plugin-top-level-await"
 
 export default defineConfig({
 	// The WASM must be imported as an ES module because the wasm-pack output type is "bundler"
 	// Attempts to use vite-plugin-wasm-pack have failed regarding HMR
-	plugins: [wasm()],
+	// vite-plugin-top-level-await is needed because vite-plugin-wasm uses top level awaits, which aren't widely supported yet
+	plugins: [wasm(), topLevelAwait()],
 })
