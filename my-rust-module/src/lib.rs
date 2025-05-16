6417f8fc4c5d35f3ab53ba7090faa1dc8c3a1e13
solarkraft <>
2025-05-16 06:42:46 +0200

    Nicer greeting messages

--- my-rust-module/src/lib.rs
+++ my-rust-module/src/lib.rs
@@ -4,10 +4,13 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn hello_console() {
-    log!("Hello from Rust!");
+    // See https://docs.rs/gloo-console/latest/gloo_console/
+    // let object = JsValue::from("any JsValue can be logged");
+
+    log!("Hello from Rust! You can find this code in my-rust-module/src/lib.rs. ")
 }
 
 #[wasm_bindgen]
 pub fn hello_alert() {
-    alert("Hello from Rust!");
+    alert("Hello from Rust! You can find this code in my-rust-module/src/lib.rs. ");
 }
